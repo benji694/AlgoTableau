16ef7e2c9eb4a818f2827a7ea97d5c29230d5406
benji694 <bbouillard@edenschool.fr>
2022-03-16 11:16:26 +0100

    tests and functions minimum, moyenne and without

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-console.log('hello')
+export const minimum = (Array)=>{
+    let min = Array[0];
+    for(let i = 0; i < Array.length; i++){
+        if(Array[i] < min){
+            min = Array[i];
+        }
+    }
+    return min; 
+}
+
+export const moyenne = (Array)=>{
+    let moy = 0;
+    for(let i = 0; i < Array.length; i++){
+        moy += Array[i];
+    }
+    return moy/Array.length;
+}
+
+export const withoutString = (Array)=>{
+    let newArr = [];
+    for(let i = 0; i < Array.length; i++){
+        if(typeof Array[i] == 'number'){
+            newArr.push(Array[i]);
+        }
+    }
+    return newArr;
+}
--- test/test.js
+++ test/test.js
@@ -1,19 +1,76 @@
 import assert from 'assert';
 
 
-describe('Int', function () {
+import { minimum } from '../js/app.js';
+describe('Array', function () {
   describe('minimum()', function () {
-    it('should return -2', function () {
-      assert.equal(minimum([-2, -5, -8, -1]), -8);
+    it('should return -8', function () {
+        assert.equal(minimum([-2, -5, -8, -1]), -8);
     });
     it('should return 2', function () {
         assert.equal(minimum([2, 5, 8, 4]), 2);
-      });
-      it('should return -2', function () {
+    });
+    it('should return -2', function () {
         assert.equal(minimum([-2, 5, 0, 8, 1]), -2);
-      });
-      it('should return -2', function () {
-        assert.equal(minimum([]), -2);
-      });
+    });
+    it('should return undifined', function () {
+        assert.equal(minimum([]), undefined);
+    });
   });
 });
+
+import { moyenne } from '../js/app.js';
+describe('Array', function () {
+    describe('moyenne()', function () {
+      it('should return -4', function () {
+          assert.equal(moyenne([-2, -5, -8, -1]), -4);
+      });
+      it('should return 3.5', function () {
+          assert.equal(moyenne([2, 5, 8, 4, -2, 4]), 3.5);
+      });
+      it('should return 4', function () {
+          assert.equal(moyenne([-2, 10]), 4);
+      });
+      it('should return Nan', function () {
+          assert.equal(moyenne([]), NaN);
+      });
+    });
+  });
+
+import { withoutString } from '../js/app.js';
+describe('Array', function () {
+    describe('withoutString()', function () {
+      it('should return [-2, -5, -1]', function () {
+          assert.deepEqual(withoutString([-2, -5, "hello", -1]), [-2, -5, -1]);
+      });
+      it('should return [2, 8, 4, 4]', function () {
+          assert.deepEqual(withoutString([2, 'hello', 8, 4, 'bonjour', 4]), [2, 8, 4, 4]);
+      });
+      it('should return [10]', function () {
+          assert.deepEqual(withoutString(['hey', 10]), [10]);
+      });
+      it('should return []', function () {
+          assert.deepEqual(withoutString([]), []);
+      });
+    });
+  });
+
+import { pairImpair } from '../js/app.js';
+describe('Array', function () {
+    describe('pairImpair()', function () {
+      it('should return [-2, -5, -1]', function () {
+          assert.deepEqual(pairImpair([-5, -10, -8, -3]), [[-5, -3],[-10, -8]]);
+      });
+      it('should return [2, 8, 4, 4]', function () {
+          assert.deepEqual(pairImpair([2, 4, 7, 9, 8]), [[7, 9],[2, 4, 8]]);
+      });
+      it('should return [10]', function () {
+          assert.deepEqual(pairImpair(['hey', 10]), [10]);
+      });
+      it('should return []', function () {
+          assert.deepEqual(pairImpair([]), []);
+      });
+    });
+  });
+
+  
